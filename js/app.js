0ce23af75f953392f5f510719250f99350eaf7b8
ragnar-pwninskjold <seankochel@gmail.com>
2016-07-11 16:47:05 -0400

    simplified to one 'for' loop

--- js/app.js
+++ js/app.js
@@ -28,14 +28,14 @@ function getRequest(searchTerm, token){
   url = 'https://www.googleapis.com/youtube/v3/search';
 
   $.getJSON(url, params, function(data){
-    var tNails = [];
-    var vidId = [];
-    var channel = [];
+    var tNails;
+    var vidId;
+    var channel;
     for (i=0;i<data.items.length;i++) {
     if (data.items[i].id.kind == "youtube#video") {
-      tNails.push(data.items[i].snippet.thumbnails.default.url);
-      vidId.push(data.items[i].id.videoId);
-      channel.push(data.items[i].snippet.channelId);
+      tNails = data.items[i].snippet.thumbnails.default.url;
+      vidId = data.items[i].id.videoId;
+      channel = data.items[i].snippet.channelId;
       nextPage = data.nextPageToken;
       console.log(nextPage);
       showResults(tNails, vidId, channel);
@@ -52,8 +52,6 @@ function getRequest(searchTerm, token){
 
 function showResults(results, vidId, cId){
   var html = "";
-  $.each(results, function(index,value){
-    html += '<div class="result-item"><a href="https://www.youtube.com/watch?v='+ vidId[index] +'">'+'<img id="resultpics" src="' + value + '">' + '</a>' + '<a href="https://www.youtube.com/channel/' + cId[index] + '">Click Here for More From This Channel</a></div>';
-  });
-  $('#search-results').html(html);
+  html += '<div class="result-item"><a href="https://www.youtube.com/watch?v='+ vidId +'">'+'<img id="resultpics" src="' + results + '">' + '</a>' + '<a href="https://www.youtube.com/channel/' + cId + '">Click Here for More From This Channel</a></div>';
+  $('#search-results').append(html);
 }
